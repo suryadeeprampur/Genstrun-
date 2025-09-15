a254f0c68a85481b393699040ba3b1e415cf297c
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-09-15 09:03:40 +0530

    Create genstring.py

---
+++ genstring.py
@@ -0,0 +1,8 @@
+from pyrogram import Client
+
+API_ID = int(input("Enter API ID: "))
+API_HASH = input("Enter API HASH: ")
+
+with Client(":memory:", api_id=API_ID, api_hash=API_HASH) as app:
+    print("\n✅ Your STRING_SESSION:\n")
+    print(app.export_session_string())
